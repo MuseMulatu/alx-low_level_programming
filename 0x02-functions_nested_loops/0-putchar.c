6a823f3c0e0dbca0a5339485013179a3ab3e922e
MuseMulatu <musemulatu.g@gmail.com>
2022-06-07 14:15:01 -0700

    nested functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h";                                                                                                                                
+                                                                                                                                                
+int _putchar(char c)                                                                                                                            
+	                                                                                                                                                
+{                                                                                                                                               
+	                                                                                                                                                
+	                printf("_putchar"n)                                                                                                             
+				                                                                                                                                                
+	return 0;                                                                                               
+			                                                                                                                                                
+			                                                                                                                                                
+}                                                                                                                                               
+                                                                                                                                                
+ 
