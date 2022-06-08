dd2834cf6f57504c49aca99ff085323ed36ad13d
MuseMulatu <musemulatu.g@gmail.com>
2022-06-08 01:06:31 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#include "main.h";                                                                                                                                
-                                                                                                                                                
-int _putchar(char c)                                                                                                                            
-	                                                                                                                                                
-{                                                                                                                                               
-	                                                                                                                                                
-	                printf("_putchar"n)                                                                                                             
-				                                                                                                                                                
-	return 0;                                                                                               
-			                                                                                                                                                
-			                                                                                                                                                
-}                                                                                                                                               
-                                                                                                                                                
- 
+#include "main.h";                                                                                                                               
+int main(void)                                                                                                                         
+{
+char str[] = "_putchar\n";
+int i=0;
+while (str[i] != '\0')
+{
+	char c = str[i];
+	_putchar(c);
+	i++;
+}
+return (0);                                                                                              
+}                                                                                                                                                
