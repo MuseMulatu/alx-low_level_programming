232c4b14c624ace2165dc6fd44307c51814479de
MuseMulatu <musemulatu.g@gmail.com>
2022-06-08 01:25:56 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include "main.h" 
+#include "main.h"
 /**
  * main -Entry point
  * Description: putchar '_putchar \n'
  * Return: 0
  */
-int main(void)                                                                                                                         
+int main(void)
 {
 char str[] = "_putchar\n";
-int i=0;
+int i = 0;
 while (str[i] != '\0')
 {
 	char c = str[i];
 	_putchar(c);
 	i++;
 }
-return (0);                                                                                              
-}                                                                                                                                                
+return (0);
+}
