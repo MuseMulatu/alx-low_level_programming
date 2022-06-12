5fa280457a3f2fb94620863b12f5759378f4a07c
MuseMulatu <musemulatu.g@gmail.com>
2022-06-12 03:07:30 -0700

    6th

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ ** _abs - Entry poin
+ ** @n: n is an integer
+ ** Description: prints the absolute value of an intege
+ ** Return: int
+ **/
+
+int _abs(int n)
+
+{
+
+if (n > 0)
+
+{
+
+return (n);
+
+}
+
+else if (n < 0)
+
+{
+
+return (-n);
+
+}
+
+else
+
+{
+
+return (n);
+
+}
+
+}
