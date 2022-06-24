a3d70c43bb503b5a7df8c8c1d6c027b948caa496
MuseMulatu <musemulatu.g@gmail.com>
2022-06-24 09:34:48 -0700

    all

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ ** main - prints all arguments passed into mai
+ ** @argc: number of command line arguments
+ ** @argv: array name
+ ** Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
